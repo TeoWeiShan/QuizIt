939e275e1559382d2c3dc28caf7b7ac82b98e271
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-02-10 22:04:07 +0800

    Add quiz html and css

---
+++ js/quiz-temp.js
@@ -0,0 +1,10 @@
+//temp testing only for loader and quiz layout
+setTimeout(
+    function()
+    { 
+        loader.classList.add('hidden');
+        game.classList.remove('hidden');
+    }, 
+3000);
+    
+    
