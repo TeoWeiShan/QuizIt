d01a6a5caf0c395c4d4822487f4b26a930e120d1
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-02-17 00:27:12 +0800

    Add clear function

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -6,3 +6,14 @@ pointData.forEach(initData);
 function initData(pointData){
   leaderboardList.innerHTML += `<li class="high-score">${pointData.dateTime} - ${pointData.point}</li>`
 }
+//Hide clear button if nothing in local storage
+if (pointData.length == 0){
+  ($('#clrBtn')[0]).classList.add('hidden');
+}
+//Clear local storage
+clrStorage = function(e) {
+    e.preventDefault();
+    localStorage.removeItem("pointData");
+    leaderboardList.innerHTML = '';
+    ($('#clrBtn')[0]).classList.add('hidden');}
+
